bdcddcf5287ccf413f5308cb540a79fe559bf064
AyomidePeggie <Shobandedeborah@gmail.com>
2024-12-20 10:36:46 +0100

    Changes to program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,6 @@
 ﻿using MyWaalureProject;
 
-var questions = new List<Question>
-{
-new Question
-{
-    Text = "How many seconds make a minute?",
-    Options = new List<string>{"60 Seconds", "90seconds", "25seconds" },
-    CorrectAnswerIndex = 0
-},
-new Question
-{
-    Text = "How many planets do we have in the world?",
-    Options = new List<string>{"16", "8", "12" },
-    CorrectAnswerIndex = 1
-},
-new Question
-{
-    Text = "What is the symbol of Helium?",
-    Options = new List<string>{"he", "h", "He" },
-    CorrectAnswerIndex = 2
-},
-new Question
-{
-    Text = "What is the past tense of go?",
-    Options = new List<string>{"camed", "went", "goed" },
-    CorrectAnswerIndex = 1 
-}
-};
+
+var questions = Quiz.Question();
 Quiz quiz = new Quiz(questions);
 quiz.StartQuiz();
--- Quiz.cs
+++ Quiz.cs
@@ -14,7 +14,7 @@ namespace MyWaalureProject
         {
 
             Random random = new Random();
-            _questions=questions.OrderBy(q =>  random.Next()).ToList();
+            _questions= questions.OrderBy(q =>  random.Next()).ToList();
             _score = 0;
         }
         public void StartQuiz()
@@ -23,6 +23,7 @@ namespace MyWaalureProject
             int questionnumber = 1;
             foreach (var question in _questions)
             {
+                Console.Clear();
                 Console.WriteLine($"\nQuestion {questionnumber}: {question.Text}");
                 
 
@@ -43,10 +44,10 @@ namespace MyWaalureProject
                         Console.WriteLine("You are correct");
                         _score++;
                     }
-                    else
-                    {
-                        Console.WriteLine($"incorrect answer selected.The correct answer is :{question.Options[question.CorrectAnswerIndex]}");
-                    }
+                    //else
+                    //{
+                    //    Console.WriteLine($"incorrect answer selected.The correct answer is :{question.Options[question.CorrectAnswerIndex]}");
+                    //}
                 }
                 else
                 {
@@ -58,6 +59,74 @@ namespace MyWaalureProject
             DisplayResult(); 
         }
 
+        public static List<Question> Question()
+        {
+            var questions = new List<Question>
+{
+new Question
+{
+    Text = "How many seconds make a minute?",
+    Options = new List<string>{"60 Seconds", "90seconds", "25seconds" },
+    CorrectAnswerIndex = 0
+},
+new Question
+{
+    Text = "How many planets do we have in the world?",
+    Options = new List<string>{"16", "8", "12" },
+    CorrectAnswerIndex = 1
+},
+new Question
+{
+    Text = "What is the symbol of Helium?",
+    Options = new List<string>{"he", "h", "He" },
+    CorrectAnswerIndex = 2
+},
+new Question
+{
+    Text = "What is the past tense of go?",
+    Options = new List<string>{"camed", "went", "goed" },
+    CorrectAnswerIndex = 1
+},
+new Question
+{
+    Text = "How long does it takes to boil an egg?",
+    Options = new List<string>{"60 Seconds", "90seconds", "25seconds" },
+    CorrectAnswerIndex = 0
+},
+new Question
+{
+    Text = "What do you call a baby goat?",
+    Options = new List<string>{"mice", "kid", "sailor" },
+    CorrectAnswerIndex = 1
+},
+new Question
+{
+    Text = "Anchor is associated to what?",
+    Options = new List<string>{"ship", "train", "airplane" },
+    CorrectAnswerIndex = 0
+},
+new Question
+{
+    Text = "How many players make a team in the game of football?",
+    Options = new List<string>{"12", "15", "11" },
+    CorrectAnswerIndex = 2
+},
+new Question
+{
+    Text = "Car is associated to park, while airplane is associated to?",
+    Options = new List<string>{"hanger", "motorpark", "parkview" },
+    CorrectAnswerIndex = 0
+},
+new Question
+{
+    Text = "What is the name of Manchester United stadium?",
+    Options = new List<string>{"Ethiad", "blue Arena", "Old Trafford" },
+    CorrectAnswerIndex = 2
+},
+};
+
+            return questions;
+        }
 
      public void DisplayResult()
         {
